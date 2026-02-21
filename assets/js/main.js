880379f394ed22ea4c37f102ea7769a52cb28c81
Ivar Sidorsson <ivar@birdisland.studio>
2026-02-21 19:09:04 +0100

    added scrollspy highlight

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            const id = entry.target.getAttribute('id');
+            if (entry.intersectionRatio > 0)
+            {
+                document.querySelector(`.navbar-entry a[href="#${id}"]`).parentElement.classList.add('active');
+            }
+            else
+            {
+                document.querySelector(`.navbar-entry a[href="#${id}"]`).parentElement.classList.remove('active');
+            }
+        });
+    })
+
+    document.querySelectorAll('section[id]').forEach(section => {
+        observer.observe(section);
+    });
+});
